c6382b471c686df2d53271865dbe9d35e9f4493d
martingaw11 <martingaw11@gmail.com>
2023-04-11 11:23:36 -0500

    Create main file and add message components

---
+++ main.js
@@ -0,0 +1,17 @@
+/*  Martin Gawron
+    4/11/2023
+    
+    Random Message Generator
+*/
+
+//THESE ARE THE MESSAGE COMPONENTS TO BE STORED
+//Market generally is made up of these 11 sectors
+const marketSectors = ['Health Care', 'Materials', 'Energy', 'Consumer Discretionary', 'Consumer Staples', 'Industrials', 'Utilities', 'Financials', 'Information Technology', 'Communication Services', 'Real Estate'];
+
+//Any part of the market has the potential to move according to these phrases
+const potentialMove = ['Very Bearish', 'Slightly Bearish', 'Neutral', 'Slightly Bullish', 'Very Bullish', 'To the Moon'];
+
+//What aspect or scope of the sector/market will be moving
+const scope = ['The entire market', 'This entire sector', 'A stock in this sector', 'Small-cap stocks in this sector', 'Large-cap stocks in this sector'];
+
+
